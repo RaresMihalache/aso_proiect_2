887af71326ba2f8191f02849fbd217b3ad1dea24
Rares Mihalache <raresmihalache@yahoo.com>
2022-12-04 23:21:24 +0200

    chat app - main functionality

---
+++ djangochat/chat/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('', views.home, name='home'),
+    path('room/<str:room>/', views.room, name='room'),
+    path('checkview', views.checkview, name='checkview'),
+    path('send', views.send, name='send'),
+    path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
+    path('register/', views.registerPage, name='register'),
+]
